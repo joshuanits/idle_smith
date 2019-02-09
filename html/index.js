540456fbfde5fef3839361486035effc435592a8
Unknown <joshuanits@gmail.com>
2019-02-10 10:14:36 +1030

    Added ores, bars and smelting

--- html/index.js
+++ html/index.js
@@ -2,10 +2,72 @@
 
 const { ipcRenderer } = require('electron')
 
+// close button
 document.getElementById('close').addEventListener('click', () => {
     ipcRenderer.send('quit')
 })
 
+// minimize button
 document.getElementById('minimize').addEventListener('click', () => {
     ipcRenderer.send('minimize')
+})
+
+// maximize button
+document.getElementById('maximize').addEventListener('click', () => {
+    ipcRenderer.send('toggle_maximize')
+})
+
+document.getElementById('smelting_start').addEventListener('click', () => {
+    const oreSelect = document.getElementById('smelting_select')
+    const oreId = oreSelect.children[oreSelect.selectedIndex].getAttribute('data-ore-id')
+
+    console.log(oreId)
+    ipcRenderer.send('smelting_start', oreId)
+})
+
+ipcRenderer.on('ores_updated', (e, ores) => {
+    // the list of the ores the player has
+    const oresList = document.getElementById('ore_list')
+    oresList.innerHTML = Object.keys(ores).reduce(function (html, key) {
+        html += `<li>${key}: ${ores[key]}</li>`
+        return html
+    }, '')
+
+    // the ores in the dropdown box for smelting
+    var listHtml = Object.keys(ores).reduce(function (html, key) {
+        if(ores[key] != 0) {
+            html += `<option data-ore-id="${key}">${key} (${ores[key]})</option>`
+        }
+        return html
+    }, '')
+
+    // button that the player clicks to start smelting. disable it if they don't have any ores
+    const smeltingButton = document.getElementById('smelting_start')
+    smeltingButton.disabled = listHtml ? false : true
+    
+    // drop down list of ores
+    const smeltingSelect = document.getElementById('smelting_select')
+    smeltingSelect.innerHTML = listHtml || '<option>None</option>' // if the list was empty, make it say none
+
+})
+
+ipcRenderer.on('smelting_updated', (e, smelting) => {
+    const smeltingText = document.getElementById('smelting_text')
+    smeltingText.innerHTML = smelting.bar || 'Nothing'
+
+    const smeltingBar = document.getElementById('smelting_bar')
+    const barWidth = smelting.progress * 100
+    smeltingBar.setAttribute('style', `width:${barWidth}%`)
+    smeltingBar.setAttribute('aria-valuenow', barWidth)
+})
+
+ipcRenderer.on('bars_updated', (e, bars) => {
+    const barsList = document.getElementById('bars_list')
+
+    var html = Object.keys(bars).reduce(function (html, key) {
+        html += `<li>${key}: ${bars[key]}</li>`
+        return html
+    }, '')
+
+    barsList.innerHTML = html
 })
--- main.js
+++ main.js
@@ -2,25 +2,90 @@
 
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+
+// todo: make this into an actual thing. for now this works, so thats good enough.
+var data = {
+    money: 0,
+    xp: 0,
+    ores: {
+        bronze: 1,
+        iron: 2,
+        steel: 3,
+    },
+    bars: {
+        bronze: 0,
+        iron: 0,
+        steel: 0
+    },
+    smelting: {
+        active: false,
+        bar: '',
+        quantity: 0,
+        progress: 0
+    }
+}
+
+// main game loop
+const gameLoop = () => {
+    if(mainWindow === null) return
+
+    if(data.smelting.active) {
+        data.smelting.progress += 3 / 100;
+        if(data.smelting.progress >= 1) {
+            // finish smelting
+            data.bars[data.smelting.bar] += data.smelting.quantity
+
+            data.smelting.active = false
+            data.smelting.bar = ''
+            data.smelting.quantity = 0
+            data.smelting.progress = 0
+
+            mainWindow.send('bars_updated', data.bars)
+        }
+
+        mainWindow.send('smelting_updated', data.smelting)
+    }
+}
+
+ipcMain.on('smelting_start', (e, oreId) => {
+    if(!data.smelting.active && data.ores[oreId] > 0) {
+        data.smelting.active = true
+        data.smelting.bar = oreId
+        data.smelting.quantity = 1
+
+        data.ores[oreId] -= 1
+
+        mainWindow.send('ores_updated', data.ores)
+    }
+})
+
 let mainWindow
 
-function createWindow() {
+const createWindow = () => {
     mainWindow = new BrowserWindow({
         show: false,
         frame: false
     })
 
-    mainWindow.loadFile(path.join("html", "index.html"))
+    mainWindow.loadFile(path.join('html', 'index.html'))
+    mainWindow.setMovable(true) // since the window is frameless need to make it so we can drag it around
 
     mainWindow.on('ready-to-show', () => {
         //mainWindow.maximize()
         mainWindow.show()
+        mainWindow.send('ores_updated', data.ores)
+        mainWindow.send('bars_updated', data.bars)
+        mainWindow.send('smelting_updated', data.smelting)
+        setInterval(gameLoop, 10)
     })
 
     mainWindow.on('closed', () => {
+        clearInterval(gameLoop)
         mainWindow = null
     })
 }
+
+// window functions
 
 app.on('ready', createWindow)
 
@@ -30,4 +95,9 @@ ipcMain.on('quit', () => {
 
 ipcMain.on('minimize', () => {
     mainWindow.minimize();
+})
+
+ipcMain.on('toggle_maximize', () => {
+    if(mainWindow.isMaximized()) mainWindow.unmaximize()
+    else mainWindow.maximize()
 })
